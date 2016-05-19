55ee0ff3f2e125627bca2fda84978bdf6b065198
Jeroen Arnoldus <jeroen@repleo.nl>
2016-05-20 00:53:48 +0200

    certificate owner added

---
+++ bounca/api/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+
+class MyUserPermissions(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if view.action == 'list':
+            return request.user.is_admin
+        elif view.action == 'retrieve':
+            return True
+        else:
+            return False
+
+    def has_object_permission(self, request, view, obj):
+        if view.action == 'retrieve':
+            return request.user.is_admin or obj == request.user                                                                       
+        else:                                                                                       
+            return False
+        
