cb27f0348d674f61743549a9523a8a41141ac758
Jeroen Arnoldus <jeroen@repleo.nl>
2016-05-22 14:06:54 +0200

    Removed redundant javascript files

--- bounca/static/libs/angular-ui-bootstrap/template/carousel/slide.html.js
+++
@@ -1,7 +0,0 @@
-angular.module("uib/template/carousel/slide.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("uib/template/carousel/slide.html",
-    "<div ng-class=\"{\n" +
-    "    'active': active\n" +
-    "  }\" class=\"item text-center\" ng-transclude></div>\n" +
-    "");
-}]);
--- bounca/static/libs/angular-ui-bootstrap/template/pager/pager.html.js
+++
@@ -1,8 +0,0 @@
-angular.module("uib/template/pager/pager.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("uib/template/pager/pager.html",
-    "<ul class=\"pager\">\n" +
-    "  <li ng-class=\"{disabled: noPrevious()||ngDisabled, previous: align}\"><a href ng-click=\"selectPage(page - 1, $event)\">{{::getText('previous')}}</a></li>\n" +
-    "  <li ng-class=\"{disabled: noNext()||ngDisabled, next: align}\"><a href ng-click=\"selectPage(page + 1, $event)\">{{::getText('next')}}</a></li>\n" +
-    "</ul>\n" +
-    "");
-}]);
