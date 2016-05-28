3f54e545c6c686a64791e4d04ba44c9b3bd49b01
Jeroen Arnoldus <jeroen@repleo.nl>
2016-05-28 14:21:05 +0200

    fixed relative path issue

--- bounca/certificate_engine/templates/ssl/sign_intermediate_cert.sh
+++ bounca/certificate_engine/templates/ssl/sign_intermediate_cert.sh
@@ -7,18 +7,24 @@ then
     echo "Please, provide number of days the certificate will be valid as first argument"
 fi
 
+#Step into root CA dir
+cd ..
 
 passphrase_in=""
-if [ -e "./passphrase_in.txt" ]
+if [ -e "./{{ cert.shortname }}/passphrase_in.txt" ]
 then
-    passphrase_in="-passin file:./passphrase_in.txt"
+    passphrase_in="-passin file:./{{ cert.shortname }}/passphrase_in.txt"
 fi
 
-openssl ca -batch -config ../openssl.cnf -extensions v3_intermediate_ca \
+
+openssl ca -batch -config ./openssl.cnf -extensions v3_intermediate_ca \
 	  -days $1 -notext -md sha256 \
-      -in ./csr/{{ key_name }}.csr.pem \
+      -in ./{{ cert.shortname }}/csr/{{ key_name }}.csr.pem \
       $passphrase_in \
-      -out ./certs/{{ key_name }}.cert.pem
+      -out ./{{ cert.shortname }}/certs/{{ key_name }}.cert.pem
+
+cd {{ cert.shortname }}
+
 
 chmod 444 ./certs/{{ key_name }}.cert.pem
 
