5a8336f1a44b6c2851fd68183c5defbfc277463e
Jeroen Arnoldus <jeroen@repleo.nl>
2021-11-22 22:51:40 +0100

    version front

--- make-package.sh
+++ make-package.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash -ex
 
 
-BASEDIR=`dirname "$0"`/
-WORKDIR="$BASEDIR/out/"
+BASEDIR=$(dirname "$0")
+WORKDIR="$BASEDIR/out/bounca"
+VERSION=$(git rev-parse --abbrev-ref HEAD)
 
 cd $BASEDIR
 #./run-checks.sh
@@ -17,10 +18,16 @@ git archive master | tar -x -C $WORKDIR
 cd $WORKDIR
 rm -rf ./vuetifyforms
 cd front
+sed -e s/\"version\":\ \"dev\"/\"version\":\ \"$VERSION\"/g package.json
 npm install
 npm run build --production
 rm -rf node_modules public src
-rm *
+rm .babelrc .env .env.production .eslintignore .eslintrc.js .postcssrc.js
+rm Makefile README.md babel.config.js package-lock.json package.json vue.config.js
+
+cd "../.."
+tar -czvf "bounca-$VERSION.tar.gz" bounca/
+
 #cd ~/tmp/
 #rm bounca-password.zip | true
 #zip -P blox bounca-password.zip bounca.zip
