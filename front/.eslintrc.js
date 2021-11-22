0f659b9242797970e1d8e314eb2e54e717ae7dd7
Jeroen Arnoldus <jeroen@repleo.nl>
2021-11-22 23:45:43 +0100

    update packages

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,19 +1,20 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-    root: true,
-    parserOptions: {
-        parser: 'babel-eslint'
-    },
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
   env: {
     browser: true,
   },
   // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  // consider switching to `plugin:vue/strongly-recommended` or
+  // `plugin:vue/recommended` for stricter rules.
   extends: ['plugin:vue/essential', 'airbnb-base'],
   // required to lint *.vue files
   plugins: [
-    'vue'
+    'vue',
   ],
 
   // add your custom rules here
@@ -24,7 +25,7 @@ module.exports = {
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       js: 'never',
-      vue: 'never'
+      vue: 'never',
     }],
     // disallow reassignment of function parameters
     // disallow parameter object manipulation except for specific exclusions
@@ -33,14 +34,14 @@ module.exports = {
       ignorePropertyModificationsFor: [
         'state', // for vuex state
         'acc', // for reduce accumulators
-        'e' // for e.returnvalue
-      ]
+        'e', // for e.returnvalue
+      ],
     }],
     // allow optionalDependencies
     // 'import/no-extraneous-dependencies': ['error', {
     //   optionalDependencies: ['test/unit/index.js']
     // }],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
