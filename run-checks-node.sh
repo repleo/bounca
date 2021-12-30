73cbcbe6dafa65a9e3a7e66c220b331f11c7fa91
Jeroen Arnoldus <jeroen@repleo.nl>
2021-12-30 14:26:15 +0100

    Spilt check node / python

---
+++ run-checks-node.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash -x
+
+BASEDIR=`dirname "$0"`
+cd $BASEDIR
+
+BLUE='\033[1;34m'
+BRIGHT_RED='\033[1;31m'
+DARK_GRAY='\033[0;37m'
+RESET='\033[m'
+INFO="${BLUE}[INFO]${RESET}"
+
+function task() {
+  if [ "$3" != "" ]; then
+    printf "$2\n"
+
+    $1 $3 &
+    pid=$!
+    wait $pid
+
+    if [ $? == 0 ]; then
+      echo -ne "\033[0K\r"
+    else
+      printf "${BRIGHT_RED}Please fix the above errors ${RESET}\n"
+      exit 1
+    fi
+  fi
+}
+
+PY_MESSAGE="${INFO} Linting javascript files ${DARK_GRAY}[*.js]${RESET}"
+cd front
+task "npm run lint" "$PY_MESSAGE" .
--- run-checks.sh
+++ run-checks.sh
@@ -1,39 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -x
 
-PYTHON_FOLDERS='api bounca certificate_engine vuetifyforms x509_pki'
-BASEDIR=`dirname "$0"`
-cd $BASEDIR
-
-BLUE='\033[1;34m'
-BRIGHT_RED='\033[1;31m'
-DARK_GRAY='\033[0;37m'
-RESET='\033[m'
-INFO="${BLUE}[INFO]${RESET}"
-
-function task() {
-  if [ "$3" != "" ]; then
-    printf "$2\n"
-
-    $1 $3 &
-    pid=$!
-    wait $pid
-
-    if [ $? == 0 ]; then
-      echo -ne "\033[0K\r"
-    else
-      printf "${BRIGHT_RED}Please fix the above errors ${RESET}\n"
-      exit 1
-    fi
-  fi
-}
-
-py_files=$(find $PYTHON_FOLDERS -path '**/migrations' -prune -o -name '*.py' -print)
-PY_MESSAGE="${INFO} Check import order on python files ${DARK_GRAY}[*.py]${RESET}"
-task "isort --check-only" "$PY_MESSAGE" "$py_files"
-PY_MESSAGE="${INFO} Linting python files ${DARK_GRAY}[*.py]${RESET}"
-task flake8 "$PY_MESSAGE" .
-PY_MESSAGE="${INFO} Black python files ${DARK_GRAY}[*.py]${RESET}"
-task "black --check" "$PY_MESSAGE" .
-PY_MESSAGE="${INFO} Linting javascript files ${DARK_GRAY}[*.js]${RESET}"
-cd front
-task "npm run lint" "$PY_MESSAGE" .
+./run-checks-python.sh
+./run-checks-node.sh
