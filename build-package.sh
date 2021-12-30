5c9b075332ffd27ecc2d64c780acbff356de500d
Jeroen Arnoldus <jeroen@repleo.nl>
2021-12-30 19:23:49 +0100

    update gitlab ci

--- build-package.sh
+++ build-package.sh
@@ -10,18 +10,18 @@ cd front
 
 cd ..
 
-mkdir out
-mv api out/
-mv bounca out/
-mv certificate_engine out/
-mv etc out/
-mv front out/
-mv logs out/
-mv x509_pki out/
-mv CONTRIBUTING.md out/
-mv LICENSE out/
-mv README.md out/
-mv changelog.md out/
-mv requirements.txt out/
+mkdir -P out/bounca/
+mv api out/bounca/
+mv bounca out/bounca/
+mv certificate_engine out/bounca/
+mv etc out/bounca/
+mv front out/bounca/
+mv logs out/bounca/
+mv x509_pki out/bounca/
+mv CONTRIBUTING.md out/bounca/
+mv LICENSE out/bounca/
+mv README.md out/bounca/
+mv changelog.md out/bounca/
+mv requirements.txt out/bounca/
 
 tar -czvf "bounca.tar.gz" --owner=0 --group=0 -C out .
